80eb8b15035596a931291cbc22ecb024027e3a04
fauji2317 <114175922+fauji2317@users.noreply.github.com>
2025-04-09 21:23:42 +0700

    sgame, spilihlokal, spsekitar

---
+++ lib/screens/spilihlokal.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class SpilihLokal extends StatelessWidget {
+  const SpilihLokal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, // Latar belakang putih
+      appBar: AppBar(
+        title: const Text('Spilih Lokal'),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 1,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: List.generate(4, (index) {
+            return Container(
+              margin: const EdgeInsets.only(bottom: 16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 4,
+                    offset: const Offset(2, 2),
+                  ),
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Image.asset(
+                  'assets/images/splokal${index + 1}.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
